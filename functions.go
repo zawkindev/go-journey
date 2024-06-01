04c17f04587106b2466aca6ee27130384109bb26
zawkindev <zawkindev@gmail.com>
2024-06-01 15:47:51 +0500

    feat: functions

--- functions.go
+++ functions.go
@@ -18,6 +18,20 @@ var say = func(txt string) {
 	fmt.Printf("%s", txt)
 }
 
+func partialSum(x int) func(int) int {
+	return func(y int) int {
+		return x + y
+	}
+}
+
+func tripleSum(a int) func(int) func(int) int {
+	return func(b int) func(int) int {
+		return func(c int) int {
+			return a + b + c
+		}
+	}
+}
+
 func main() {
 	x, y := calculate(3, 4)
 	fmt.Printf("area: %d, perimeter: %d\n", x, y)
@@ -32,4 +46,8 @@ func main() {
 	}("salom"),
 	)
 
+	firstSum := partialSum(5)
+	fmt.Printf("\nPARTIAL SUM: %d", firstSum(5))
+
+	fmt.Printf("\nTRIPLEsum func: %d", tripleSum(3)(4)(7))
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-journey
+
+go 1.22.3
