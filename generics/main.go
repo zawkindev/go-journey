31e333e612503086471157dd0579eb4397f628b7
zawkindev <zawkindev@gmail.com>
2024-06-21 18:17:10 +0500

    feat: generic function

--- generics/main.go
+++ generics/main.go
@@ -15,11 +15,31 @@ func (a *Asuna) Confess() string {
 	return "スキデス"
 }
 
-func printConfession[T Waifu](object T) {
+func printConfession[T Waifu](object T) { // main POINT
 	fmt.Println(object.Confess())
+}
+
+type Comparable interface {
+	~int | ~float32 | ~float64
+}
+
+func Max[T Comparable](a, b, c T) T {
+	if a > b {
+		if a > c {
+			return a
+		}
+		return c
+	} else {
+		if c > b {
+			return c
+		}
+		return b
+	}
 }
 
 func main() {
 	girlfriend := Asuna{name: "Asuna", age: 18}
 	printConfession(&girlfriend)
+	fmt.Println("Max of (3,9,1) is: ", Max(3, 9, 1))
+	fmt.Println("Max of (3.4,9.8,1) is: ", Max(3.4, 9.8, 1))
 }
