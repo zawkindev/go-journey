9609ed5fa85dbc41a3a220edbf320eaf0d51d50f
zawkindev <zawkindev@gmail.com>
2024-06-29 10:52:52 +0500

    feat: can serve file

--- server/main.go
+++ server/main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
 	"net/http"
+	"time"
 )
 
+func showInfo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Current time: ", time.Now())
+	fmt.Fprintln(w, "Url path: ", html.EscapeString(r.URL.Path))
+}
+
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
 func main() {
-	http.ListenAndServe(":8999", http.FileServer(http.Dir("/var/www")))
-	fmt.Print("salom")
+	http.HandleFunc("/", serveFile)
+	http.HandleFunc("/info", showInfo)
+
+	err := http.ListenAndServe(":8999", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
