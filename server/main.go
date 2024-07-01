a56f6bb06a49bc54e8373f87400a15bcfd996d72
zawkindev <zawkindev@gmail.com>
2024-07-01 15:46:10 +0500

    feat: midlleware

--- server/main.go
+++ server/main.go
@@ -17,13 +17,26 @@ func serveFile(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
+func logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		fmt.Printf("%s %s %v\n", r.Method, r.URL.Path, time.Since(start))
+	})
+}
+
 func main() {
-	http.HandleFunc("/", serveFile)
+
+	finalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+	})
+
+	http.HandleFunc("/", finalHandler)
 	http.HandleFunc("/info", showInfo)
+	http.Handle("/log", logger(finalHandler))
 
 	files := http.FileServer(http.Dir("C:\\Users\\shahruz\\webserver\\www"))
 	http.Handle("/files/", http.StripPrefix("/files/", files))
-
 	err := http.ListenAndServe(":8999", nil)
 	if err != nil {
 		log.Fatal(err)
