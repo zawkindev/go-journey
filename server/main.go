0becf975fb79457b3e3c9bf3c9b87dcb059e16d4
zawkindev <zawkindev@gmail.com>
2024-07-03 23:19:19 +0500

    feat: write custom Handler

--- server/main.go
+++ server/main.go
@@ -6,6 +6,15 @@ import (
 	"net/http"
 )
 
+type afterMiddleWare struct {
+	handler http.Handler
+}
+
+func (a *afterMiddleWare) serveHttp(w http.ResponseWriter, r *http.Request) {
+	a.handler.ServeHTTP(w, r)
+	w.Write([]byte("Good Night dear"))
+}
+
 func middleware1(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
@@ -27,9 +36,11 @@ func final(w http.ResponseWriter, r *http.Request) {
 func main() {
 	files := http.FileServer(http.Dir("./data/crucial"))
 
+	mid := &afterMiddleWare{http.HandlerFunc(final)}
+
 	http.Handle("/data/", http.StripPrefix("/data/", files))
 
-	http.Handle("/", middleware1(middleware2(http.HandlerFunc(final))))
+	http.Handle("/", middleware1(middleware2(mid.handler)))
 
 	err := http.ListenAndServe(":8999", nil)
 	if err != nil {
